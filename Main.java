478061a177c8a80c4a6188953f02102268a0a7b5
Dani55Alv <alvarezmdanielel62@gmai.com>
2024-12-17 12:33:58 +0100

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+        System.out.println("");
+        System.out.print("");
+        System.out.println("___");
+        System.out.println("a");
+        System.out.println("E");
+
+    }
+}
